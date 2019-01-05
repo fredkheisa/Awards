a1d3006ca52ff37392997780dd8c8c9abe0fec84
Github User Name <you@example.com>
2019-01-05 13:15:42 +0300

    onto my application views file creating and defining functions at this commit

--- vote/views.py
+++ vote/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm, ProfileForm, DesignForm, ContentForm, UsabilityForm
+from .models import Project, Profile
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfSerializer, ProjectSerializer
+from .permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
 
 # Create your views here.
